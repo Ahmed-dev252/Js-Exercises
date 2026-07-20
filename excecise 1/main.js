06e51f9e0b6b6d7b1e118a0c85f21368854fd174
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-20 15:19:05 +0300

    Submited excercise 1 & 2

---
+++ excecise 1/main.js
@@ -0,0 +1,15 @@
+// console.log("hello from the External JS file" )
+ 
+let x = 7
+let y = 3
+
+console.log (x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x++);
+console.log(x--);
+console.log(y++);
+console.log(--x);
+
