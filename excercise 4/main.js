23f30f0b60f440153e18c0108c92d22748361e09
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-21 14:40:16 +0300

    Excercise 4.

---
+++ excercise 4/main.js
@@ -0,0 +1,28 @@
+
+
+// basic
+function add (b, c){
+    console.log(b + c)
+}
+
+add(4,7);
+add(6, 10);
+add(50,60)
+
+
+
+
+
+// funstion Expressions
+const Add =function(d,f ){
+    return d + f;
+};
+
+// console.log( Multiplication(2, 5));
+console.log( Add(2, 5));
+
+
+let result =Add(6,8);
+// console.log(Multiplication);
+
+console.log(result / 2 )
