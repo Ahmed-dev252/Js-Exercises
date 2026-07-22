563de6ba805275f98b1e3f19451028bb0205b765
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-22 11:18:44 +0300

    Excercise 7

---
+++ excercise 7/main.js
@@ -0,0 +1,29 @@
+
+
+
+let colers =[ "blue", "Orange", "white" ];
+
+console.log(colers);
+
+console.log("after-push");
+
+colers.push("green");
+console.log(colers);
+
+console.log("after-pop");
+
+colers.pop();
+console.log(colers);
+
+console.log("after-unshift");
+
+colers.unshift("green");
+console.log(colers);
+
+console.log("after-shift");
+
+colers.shift();
+console.log(colers);
+
+
+console.log( colers.length);
