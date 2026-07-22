a3d2d4cc76da4d4d9e4ef6b56cfd20892945057c
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-22 14:46:45 +0300

    excercise 8

---
+++ excercise 8/main.js
@@ -0,0 +1,19 @@
+
+
+let car ={
+    
+    make: "Honda",
+    model: "Civic",
+    name: "Honda-Civic",
+    year: 2026,
+};
+
+console.log(car);
+
+console.log(car.make);
+
+
+console.log(car.name);
+
+console.log(car["year"]);
+console.log(car["model"]);
