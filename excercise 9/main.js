d24b9d2495626c41bf0f4656602d7a8fe662ab11
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-22 17:33:05 +0300

    excercise 9

---
+++ excercise 9/main.js
@@ -0,0 +1,16 @@
+
+
+let car ={
+    
+    make: "Honda",
+    model: "Civic",
+    name: "Honda-Civic",
+    year: 2026,
+
+    start : function() {
+        console.log( "The car has started")
+    }
+
+};
+
+ car.start();
