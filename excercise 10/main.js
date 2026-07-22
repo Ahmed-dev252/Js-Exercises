aa8e86ced1dad9bb0cb6dd40aa61fe3d3f0d858f
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-22 18:14:23 +0300

    excercise 10

---
+++ excercise 10/main.js
@@ -0,0 +1,27 @@
+
+
+let librarey =[
+   {    
+        Title: "Introduction to Islam",
+        Author: "Tariq Ramadan",
+        Year: 2017,
+    },
+
+   {
+        Title: "ABrief Illustrated Guide to Understanding Islam",
+        Author: "I.A.Ibrahim",
+        Year: 1997,
+    },
+
+    {
+        Title: "Secrets of Divine Love: A Spiritual Journey into the Heart of Islam",
+        Author: "A. Helwa",
+        Year: 2020,
+    },
+];
+
+console.log(librarey);
+
+console.log(librarey[0].Title);
+
+console.log(librarey[1].Author);
