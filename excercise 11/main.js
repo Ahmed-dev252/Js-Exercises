864921a23e5ee7e5a018ae523f12d8d1b064e7fe
Ahmed-dev252 <ahmedcabdinuur.me@gmail.com>
2026-07-24 10:42:50 +0300

    excercise 12

--- excercise 11/main.js
+++ excercise 11/main.js
@@ -1,5 +1,5 @@
 
 
-for (let i = 1; i < 11; i++){
+for (let i = 1; i < 51; i++){
     console.log(i)
 }
---
+++ excercise 12/main.js
@@ -0,0 +1,8 @@
+
+
+const fruites= [ "Apple", "banana", "cherry"]
+
+for (let i=0; i < fruites.length; i++ ){
+    console.log(fruites[i])
+
+};
